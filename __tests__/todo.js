98354d5efa87c247391b78db5048d950623ec440
suprabathk <suprabathkondapally@gmail.com>
2022-09-20 22:00:30 +0530

    Fixed test cases

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,36 +1,57 @@
 /* eslint-disable no-undef */
 const todo = require("../todo");
 const { all, add, markAsComplete, overdue, dueToday, dueLater } = todo();
+const today = new Date();
+const yesterday = new Date(new Date().setDate(today.getDate() - 1));
+const tommorow = new Date(new Date().setDate(today.getDate() + 1));
 
 describe("TODO test suite", () => {
   beforeAll(() => {
     add({
       title: "Wake Up",
-      dueDate: "2022-09-17",
-      completed: true,
+      dueDate: today.toLocaleDateString("en-CA"),
+      completed: false,
     });
   });
   test("Add task", () => {
     let lengthBefore = all.length;
     add({
       title: "Eat",
-      dueDate: "2022-09-17",
+      dueDate: today.toLocaleDateString("en-CA"),
       completed: false,
     });
     expect(all.length).toBe(lengthBefore + 1);
   });
   test("Mark task as complete", () => {
-    all[0].completed = false;
+    expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
   test("Over due tasks", () => {
-    expect(overdue(all)).toBeDefined();
+    const overduecount = overdue(all).length;
+    add({
+      title: "work",
+      dueDate: yesterday.toLocaleDateString("en-CA"),
+      completed: false,
+    });
+    expect(overdue(all).length > overduecount);
   });
   test("Due today tasks", () => {
-    expect(dueToday(all)).toBeDefined();
+    const duetodaycount = dueToday(all).length;
+    add({
+      title: "work1",
+      dueDate: today.toLocaleDateString("en-CA"),
+      completed: false,
+    });
+    expect(dueToday(all).length > duetodaycount);
   });
   test("Due later tasks", () => {
-    expect(dueLater(all)).toBeDefined();
+    const duelatercount = dueLater(all).length;
+    add({
+      title: "work2",
+      dueDate: tommorow.toLocaleDateString("en-CA"),
+      completed: false,
+    });
+    expect(dueLater(all).length > duelatercount);
   });
 });
